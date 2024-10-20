b745ad3c0a3570cd3292e994549a7c65e8ab802b
Rendy Pratama <rendypratama752@gmail.com>
2024-10-20 17:02:39 +0700

    update: fragment_about.xml content

--- app/src/main/java/com/example/animeapp/ui/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/animeapp/ui/fragments/AboutFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.animeapp.ui.activities.MainActivity
 import com.example.animeapp.databinding.FragmentAboutBinding
-import com.example.animeapp.ui.viewmodels.AboutViewModel
 import com.example.animeapp.ui.viewmodels.AnimeRecommendationsViewModel
 
 class AboutFragment : Fragment() {
@@ -23,16 +20,9 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewModel = (activity as MainActivity).animeRecommendationsViewModel
-        val aboutViewModel =
-            ViewModelProvider(this).get(AboutViewModel::class.java)
-
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAbout
-        aboutViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
