fe5c666619b43dc82079ab67c60313855b441dbd
Rendy Pratama <rendypratama752@gmail.com>
2024-11-01 20:52:46 +0700

    update: all imageurl attribute nullable

--- app/src/main/java/com/example/animeappkotlin/models/ImageUrl.kt
+++ app/src/main/java/com/example/animeappkotlin/models/ImageUrl.kt
@@ -4,9 +4,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ImageUrl(
-    val image_url: String,
-    val small_image_url: String,
+    val image_url: String?,
+    val small_image_url: String?,
     val medium_image_url: String?,
-    val large_image_url: String,
+    val large_image_url: String?,
     val maximum_image_url: String?
 )
