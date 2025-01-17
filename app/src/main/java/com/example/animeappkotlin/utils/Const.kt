fd260ddaf4a2f7c44b7e31e68192eaf0700c92de
Rendy Pratama <rendypratama752@gmail.com>
2025-01-17 22:24:02 +0700

    feat: make openings and endings clickable to search on youtube
    The commit makes the openings and endings in the anime detail view clickable.
    Clicking an opening or ending will open a YouTube search for that item.
    This allows users to easily find and listen to the music from the anime.

--- app/src/main/java/com/example/animeappkotlin/utils/Const.kt
+++ app/src/main/java/com/example/animeappkotlin/utils/Const.kt
@@ -3,5 +3,6 @@ package com.example.animeappkotlin.utils
 class Const {
     companion object {
         const val BASE_URL = "https://api.jikan.moe/"
+        const val YOUTUBE_URL = "https://www.youtube.com/"
     }
 }
