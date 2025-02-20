a599d79622cae00dd69120baa9573d770e8ae92f
Rendy Pratama <rendypratama752@gmail.com>
2025-02-20 22:43:31 +0700

    refactor: NetworkUtils

--- app/src/main/java/com/example/animeapp/utils/NetworkUtils.kt
+++ app/src/main/java/com/example/animeapp/utils/NetworkUtils.kt
@@ -9,10 +9,11 @@ object NetworkUtils {
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork ?: return false
+        val network = connectivityManager.activeNetwork?: return false
         val networkCapabilities =
-            connectivityManager.getNetworkCapabilities(network) ?: return false
+            connectivityManager.getNetworkCapabilities(network)?: return false
 
-        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 }
