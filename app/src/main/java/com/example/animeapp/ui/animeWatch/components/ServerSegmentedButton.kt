a0153d65134dd5b93190ffea1ac9a427b965c6ec
Rendy Pratama <rendypratama752@gmail.com>
2025-03-23 23:10:08 +0700

    add: server selection segmented button

---
+++ app/src/main/java/com/example/animeapp/ui/animeWatch/components/ServerSegmentedButton.kt
@@ -0,0 +1,37 @@
+package com.example.animeapp.ui.animeWatch.components
+
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.animeapp.models.Server
+
+@Composable
+fun ServerSegmentedButton(
+    servers: List<Server>?,
+    modifier: Modifier = Modifier
+) {
+    if (servers.isNullOrEmpty()) return
+
+    val selectedIndex: MutableState<Int> = remember { mutableIntStateOf(0) }
+    SingleChoiceSegmentedButtonRow(modifier = modifier) {
+        servers.forEachIndexed { index, server ->
+            SegmentedButton(
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = servers.size
+                ),
+                onClick = { selectedIndex.value = index },
+                selected = index == selectedIndex.value,
+                label = {
+                    Text(server.serverName)
+                }
+            )
+        }
+    }
+}
