428bc240d63187c24d2565cb022bd4aa3c67cb09
Rendy Pratama <rendypratama752@gmail.com>
2025-05-11 02:19:28 +0700

    refactor: `onScreenOff` parameter nullable in `ScreenOffReceiver`

--- app/src/main/java/com/example/animeapp/utils/ScreenOffReceiver.kt
+++ app/src/main/java/com/example/animeapp/utils/ScreenOffReceiver.kt
@@ -4,12 +4,10 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class ScreenOffReceiver(
-    private val onScreenOff: () -> Unit
-) : BroadcastReceiver() {
+class ScreenOffReceiver(private val onScreenOff: (() -> Unit)? = null) : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_SCREEN_OFF) {
-            onScreenOff()
+            onScreenOff?.invoke()
         }
     }
 }
