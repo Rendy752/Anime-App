12b9aacc4068af13d8dfbe01607ca2230aabd22c
Rendy Pratama <rendypratama752@gmail.com>
2025-05-19 19:19:30 +0700

    refactor: improve UI for AnimeScheduleItem and its skeleton

--- app/src/main/java/com/luminoverse/animevibe/ui/common/AnimeScheduleItem.kt
+++ app/src/main/java/com/luminoverse/animevibe/ui/common/AnimeScheduleItem.kt
@@ -5,14 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,90 +51,118 @@ fun AnimeScheduleItem(
                             innerPadding = PaddingValues(
                                 horizontal = 8.dp,
                                 vertical = 4.dp
-                            )
+                            ),
+                            alpha = 0.75f
                         )
                         .align(Alignment.TopStart),
                     text = it,
                     style = MaterialTheme.typography.titleSmall,
                 )
             }
-            if (remainingTime.isNotEmpty()) {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+            ) {
+                if (remainingTime.isNotEmpty()) {
+                    Text(
+                        text = remainingTime,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.titleSmall,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .basicContainer(
+                                isError = remainingTime != "On Air",
+                                isPrimary = remainingTime == "On Air",
+                                innerPadding = PaddingValues(
+                                    horizontal = 8.dp,
+                                    vertical = 4.dp
+                                ),
+                                alpha = 0.75f
+                            )
+                    )
+                }
                 Text(
-                    text = remainingTime,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.titleSmall,
-                    color = MaterialTheme.colorScheme.onPrimary,
                     modifier = Modifier
-                        .align(Alignment.BottomEnd)
                         .basicContainer(
-                            isError = remainingTime != "On Air",
-                            isPrimary = remainingTime == "On Air",
+                            outerPadding = PaddingValues(0.dp),
                             innerPadding = PaddingValues(
                                 horizontal = 8.dp,
                                 vertical = 4.dp
-                            )
+                            ),
+                            alpha = 0.75f
                         )
+                        .fillMaxWidth(),
+                    text = animeDetail.title,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleSmall,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
-        Text(
-            modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-            text = animeDetail.title,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleSmall,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis
-        )
     }
 }
 
 @Preview
 @Composable
 fun AnimeScheduleItemSkeleton(modifier: Modifier = Modifier) {
-    Column(
+    Box(
         modifier = modifier
             .basicContainer(
                 outerPadding = PaddingValues(0.dp),
                 innerPadding = PaddingValues(0.dp)
             )
+            .fillMaxWidth()
     ) {
-        Box(modifier = Modifier.fillMaxWidth()) {
-            SkeletonBox(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(2f / 3f)
-            )
-            Box(
-                modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(8.dp)
-            ) {
-                SkeletonBox(
-                    modifier = Modifier.clip(RoundedCornerShape(16.dp)),
-                    width = 30.dp,
-                    height = 20.dp
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(8.dp)
-            ) {
-                SkeletonBox(
-                    modifier = Modifier.clip(RoundedCornerShape(16.dp)),
-                    width = 75.dp,
-                    height = 20.dp
-                )
-            }
-        }
         SkeletonBox(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp),
-            height = 40.dp
+                .aspectRatio(2f / 3f)
         )
+        SkeletonBox(
+            modifier = Modifier
+                .basicContainer(
+                    innerPadding = PaddingValues(
+                        horizontal = 8.dp,
+                        vertical = 4.dp
+                    ),
+                    alpha = 0.75f
+                )
+                .align(Alignment.TopStart),
+            width = 30.dp,
+            height = 20.dp
+        )
+        Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+            Column(horizontalAlignment = Alignment.End) {
+                SkeletonBox(
+                    modifier = Modifier
+                        .basicContainer(
+                            isPrimary = true,
+                            innerPadding = PaddingValues(
+                                horizontal = 8.dp,
+                                vertical = 4.dp
+                            ),
+                            alpha = 0.75f
+                        ),
+                    width = 75.dp,
+                    height = 20.dp
+                )
+                SkeletonBox(
+                    modifier = Modifier
+                        .basicContainer(
+                            outerPadding = PaddingValues(0.dp),
+                            innerPadding = PaddingValues(
+                                horizontal = 8.dp,
+                                vertical = 4.dp
+                            ),
+                            alpha = 0.75f
+                        )
+                        .fillMaxWidth(),
+                    height = 40.dp
+                )
+            }
+        }
     }
 }
