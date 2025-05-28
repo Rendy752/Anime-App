480f43d17917c87aebc18a028dc621b7f3c66e4a
Rendy Pratama <rendypratama752@gmail.com>
2025-05-28 23:00:59 +0700

    add: VideoPlayer loading indicator

--- app/src/main/java/com/luminoverse/animevibe/ui/animeWatch/videoPlayer/VideoPlayer.kt
+++ app/src/main/java/com/luminoverse/animevibe/ui/animeWatch/videoPlayer/VideoPlayer.kt
@@ -6,12 +6,16 @@ import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.util.Log
 import androidx.annotation.OptIn
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.LockOpen
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -23,7 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.PlayerView
@@ -153,10 +159,18 @@ fun VideoPlayer(
             ScreenshotDisplay(
                 imageUrl = episodeDetailComplement.imageUrl,
                 screenshot = episodeDetailComplement.screenshot,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(16f / 9f),
+                modifier = Modifier.fillMaxSize(),
                 clickable = false
+            )
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = Color.White.copy(0.5f))
+                    .padding(8.dp)
+                    .align(Alignment.Center)
+                    .size(48.dp),
+                color = Color.Black,
+                strokeWidth = 4.dp
             )
         } else {
             PlayerViewWrapper(
